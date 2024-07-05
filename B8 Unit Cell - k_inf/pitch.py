854bf79962a91a4c81b6e03b1df4708c33d7cb71
Jason Zhao <zhaoj@tamu.edu>
2024-07-05 03:17:05 -0500

    documenting pitch

--- B8 Unit Cell - k_inf/pitch.py
+++ B8 Unit Cell - k_inf/pitch.py
@@ -23,6 +23,7 @@ def to_device(data, device):
 device = get_default_device()
 
 def delaunayAdjacencyMatrix(nodes):
+    """Computes an adjacency matrix of the delaunay graph with M[i][j] -> 1 if is_adjacent(n_i, n_j) else 0"""
     n = len(nodes)
     d = Delaunay(nodes)
     adjTensor = torch.zeros(n,n,device=device)
@@ -34,6 +35,7 @@ def delaunayAdjacencyMatrix(nodes):
     return adjTensor
 
 def l2Matrix(nodes):
+    """Computes a matrix of L2 norms with M[i][j] -> ||n_i - n_j||"""
     nodesTensor = to_device(torch.Tensor(nodes), device)
     nodesL2 = nodesTensor.expand(len(nodes),len(nodes),len(nodes[0]))
     nodesL2 = nodesL2 - nodesL2.transpose(0,1)
@@ -45,4 +47,4 @@ nodesL2 = l2Matrix(NODES)
 adjMatrix = delaunayAdjacencyMatrix(NODES)
 nodesL2 = adjMatrix*nodesL2
 edges = torch.count_nonzero(nodesL2)
-print(torch.sum(nodesL2)/edges)
+print(torch.sum(nodesL2)/edges) # takes the average adjacent L2 norm
